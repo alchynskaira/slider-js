ce11c84780d353d62c74ca2a3a0390770c304af6
iryna.alchynska <iryna.alchynska@avenga.com>
2022-02-09 15:57:15 +0200

    fix drag

--- slider.js
+++ slider.js
@@ -8,32 +8,9 @@ const pictures = [
 
 const wrapper = document.getElementById("container");
 
- // wrapper.addEventListener('mousedown', handleTouchStart, false);
- // wrapper.addEventListener('mousemove', handleTouchMove, false);
-
-// let clickX  = null;
-//
-// function  handleTouchStart(e){
-//
-//     clickX = e.clientX;
-//     console.log(clickX)
-// }
-// function handleTouchMove (e){
-//
-//     console.log(clickX)
-// if(!clickX){
-//    return false;
-// }
-// let clickX2 = e.clientX;
-// let xDifference = clickX2 - clickX;
-// if(xDifference > 0) {
-//     switchSlides(true);
-// }
-//     else {
-//         switchSlides(false);
-// }
-// }
-// clickX = null;
+let direction;
+let startPosition = 0;
+let slides;
 
 
 
@@ -50,9 +27,9 @@ function renderSlides(pictures) {
     slider.append(dotBox);
 
     pictures.map((item, index) => {
-        let itemImg = `<div class="fade image-item ${index === 0 ? 'active' : ''}" data-img-index= "${index}" >
-                       <img class="slide-img" src= "${item}" alt="image">
-                       </div>`;
+        let itemImg = `<span class="fade image-item ${index === 0 ? 'active' : ''}" data-img-index= "${index}" >
+                       <img class="slide-img" fade src= "${item}" alt="image">
+                       </span>`;
 
         imgList.innerHTML += itemImg;
         dotBox.innerHTML +=  createDot(index);
@@ -61,7 +38,6 @@ function renderSlides(pictures) {
 
     createButtonWrapper()
 }
-
 
 
 function createButtonWrapper() {
@@ -123,109 +99,41 @@ function moveToSlide(event) {
     markDot(event.target.dataset?.dotIndex);
 }
 
-renderSlides(pictures);
-
-
-
-// let isDrawing = false;
-// let x = 1000;
-const slides = document.getElementsByClassName('slider')[0];
-const slides_1 = document.getElementsByClassName("image-list")[0];
-console.log(slides_1)
-
-
-// slides_1.addEventListener('mousedown', e => {
-//     x = e.offsetX;
-//     isDrawing = true;
-//     console.log(x);
-// });
-//
-// slides_1.addEventListener('mousemove', e => {
-//     if (isDrawing === true) {
-//         x = e.offsetX;
-//         slides_1.style.transform = `translate(calc(1000px - ${x}px), 0)`;
-//         console.log(x);
-//     }
-// });
-//
-// slides.addEventListener('mouseup', e => {
-//     if (isDrawing === true) {
-//         x = 0;
-//         isDrawing = false;
-//     }
-// });
-
-function slide (wrapper, items) {
-    let posX1 = null;
-
-    let posX2 = null;
-    let posInitial;
-    let posFinal;
-    let slides = items.getElementsByClassName("image-item");
-    let slidesLength = slides.length;
-    let slidesSize = items.getElementsByClassName("image-item")[0].offsetWidth;
-
-    let index = 0;
-    let isDrawing = true;
-    items.onmousedown = dragStart;
-    items.addEventListener('touchstart', dragStart);
-    items.addEventListener('touchend', dragEnd);
-    items.addEventListener('touchmove', dragAction);
-
-    //items.addEventListener("transitionend", checkIndex);
-    function  dragStart(e){
-        e.preventDefault();
-        posInitial = items.offsetLeft;
-        console.log(posInitial)
-        if (e.type == 'touchstart') {
-            posX1 = e.touches[0].clientX;
-        } else {
-            posX1 = e.clientX;
-            document.onmouseup = dragEnd;
-            document.onmousemove = dragAction;
-        }
-    }
-
-    function dragAction (e) {
-        if (e.type == 'touchmove') {
-            posX2 = posX1 - e.touches[0].clientX;
-            posX1 = e.touches[0].clientX;
-        } else {
-            posX2 = posX1 - e.clientX;
-            posX1 = e.clientX;
-        }
-        items.style.left = (items.offsetLeft - posX2) + "px";
-    }
-
-    function dragEnd (e) {
-        posFinal = items.offsetLeft;
-
-        if (posFinal - posInitial < - slidesSize) {
-            console.log('dragRight')
-            // switchSlides(true);
-        } else if (posFinal - posInitial > slidesSize) {
-            console.log("dragLeft")
-        } else {
-            items.style.left = (posInitial) + "px";
-        }
-
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-    // function checkIndex (){
-    //     items.classList.remove('shifting');
-    //
-    //     if (index == -1) {
-    //         items.style.left = -(slidesLength * slidesSize) + "px";
-    //         index = slidesLength - 1;
-    //     }
-    //
-    //     if (index == slidesLength) {
-    //         items.style.left = -(1 * slidesSize) + "px";
-    //         index = 0;
-    //     }
-    //
-    //     isDrawing = true;
-    // }
+function addListenersDrag () {
+    slides = document.getElementById("image-list");
+    slides.onmousedown = dragStart;
+    slides.addEventListener('touchstart', dragStart);
+    slides.addEventListener('touchend', dragEnd);
+    slides.addEventListener('touchmove', dragAction);
 }
-slide(slides, slides_1)
+
+function  dragStart(e){
+    e.preventDefault();
+    document.onmouseup = dragEnd;
+    document.onmousemove = dragAction;
+}
+
+function dragAction (e) {
+    if (e.pageX < startPosition) {
+        direction = "left";
+    } else if (e.pageX > startPosition) {
+        direction = "right";
+    }
+    startPosition = e.pageX;
+}
+
+function dragEnd (e) {
+    if (direction === 'right') {
+        switchSlides(false);
+    } else {
+        switchSlides(true);
+    }
+
+    document.onmouseup = null;
+    document.onmousemove = null;
+}
+renderSlides(pictures);
+addListenersDrag()
+
+
+
